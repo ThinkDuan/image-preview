d6e0a881b874e42b7b90daa855dcbc917a04292b
duanxinxin <duanxinxins@163.com>
2019-08-06 18:05:26 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <image-preview></image-preview>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ImagePreview from './components/ImagePreview.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    ImagePreview
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/ImagePreview.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="preview">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ImagePreview',
+}
+</script>
